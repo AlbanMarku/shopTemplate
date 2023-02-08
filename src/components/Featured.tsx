7e8fc91215c21486b53b3bb9e2061ceb291dd730
AlbanMarku <albanmarku@outlook.com>
2023-02-08 23:16:28 +0000

    Basic featured comp.

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,11 +1,32 @@
+import Container from 'react-bootstrap/esm/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import ItemCard from './ItemCard';
+import '../styles/featured.css';
 
 function Featured() {
+  const arr = [1, 2, 3, 'pass the props later'];
   return (
     <div className="Featured">
-      <h2>Featured products</h2>
       <div className="itemArea">
-        <ItemCard />
+        <Container fluid="lg" className="gx-0 content">
+          <Row className="gx-0">
+            <Col xs={12}>
+              <header>
+                <h2>Featured products</h2>
+              </header>
+            </Col>
+          </Row>
+          <Row className="gx-0">
+            {arr.map((item) => (
+              <Col key="t">
+                <div className="test">
+                  <ItemCard />
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </div>
     </div>
   );
